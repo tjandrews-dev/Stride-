02784a1ab96dc8c0c1bdc90e7b28e782ff325c9f
tjandrews-dev <tjandrewsracingaus@gmail.com>
2025-09-05 11:56:16 +1000

    Create page.tsx

---
+++ app/sell/page.tsx
@@ -0,0 +1,8 @@
+export default function SellPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Sell a Horse</h1>
+      <p>Here you can list your horse, tack, or transport for sale.</p>
+    </main>
+  );
+}
