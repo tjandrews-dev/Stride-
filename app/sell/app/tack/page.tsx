f71fa9a1e73688c8de8d142378aba6dc1c782997
tjandrews-dev <tjandrewsracingaus@gmail.com>
2025-09-05 11:57:08 +1000

    Create page.tsx

---
+++ app/sell/app/tack/page.tsx
@@ -0,0 +1,8 @@
+export default function TackPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Tack & Equipment</h1>
+      <p>Browse saddles, bridles, rugs, boots, and more.</p>
+    </main>
+  );
+}
